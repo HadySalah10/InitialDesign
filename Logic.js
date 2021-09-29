99f389d7755b55a2f8390d62a326d500b2fa6a57
HadySalah10 <47247169+HadySalah10@users.noreply.github.com>
2021-09-29 08:55:47 +0200

    cart js Finished

--- Logic.js
+++ Logic.js
@@ -27,3 +27,4 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
---
+++ cart.js
@@ -0,0 +1,40 @@
+function increment(id){
+    var input =document.getElementById(id);
+    var vlaueToIcrease=parseInt( input.value);
+  vlaueToIcrease++;
+  input.value=vlaueToIcrease;
+  calcTotal(vlaueToIcrease,id)
+
+  }
+  function dicrement(id){
+    var input =document.getElementById(id);
+    var vlaueToDicrease=parseInt( input.value);
+  vlaueToDicrease--;
+  if(vlaueToDicrease<0){
+    vlaueToDicrease=0;
+  }
+  input.value=vlaueToDicrease;
+  calcTotal(vlaueToDicrease,id)
+
+  }
+  function calcTotal(val,id){
+    id = id.replace("moveInput", "");
+    idPrice="idPrice"+id;
+    valueIdPrice=parseFloat(document.getElementById(idPrice).innerText);
+    valueIdTotal= document.getElementById("idTotal"+id);
+    valueIdTotal.innerText=val*valueIdPrice;
+    totalAmount();
+
+  }
+  function totalAmount(){
+    var oRows = document.getElementById('myTable').getElementsByTagName('tr');
+    var iRowCount = oRows.length-1;
+    var tot=0;
+    for (let index = 1; index <= iRowCount; index++) {
+        var totVal=parseFloat(document.getElementById("idTotal"+index).innerText);
+        tot+=totVal; 
+        
+    }
+    valueIdTotal= document.getElementById('totalAmount');
+    valueIdTotal.innerText=tot;
+  }
